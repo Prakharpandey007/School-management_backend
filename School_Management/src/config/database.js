03fd9896631e7c3b43d9aef5bf1ed038136b7543
prakhar <prakharharshit.2003@gmail.com>
2024-08-23 23:15:40 +0530

    changes done

--- School_Management/src/config/database.js
+++ School_Management/src/config/database.js
@@ -2,9 +2,9 @@ const { Sequelize } = require("sequelize");
 const config = require("../config/config.json")["development"];
 
 const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
+    process.env.DATABASE,
+    process.env.USERNAME,
+    process.env.PASSWORD,
   {
     host: config.host,
     dialect: config.dialect,
--- School_Management/src/config/serverconfig.js
+++ School_Management/src/config/serverconfig.js
@@ -3,4 +3,7 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT,
+  USERNAME:process.env.USERNAME,
+  PASSWORD:process.env.PASSWORD,
+  DATABASE:process.env.DATABASE
 };
