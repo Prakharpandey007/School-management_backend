691c2cb207814e86c545561dfcf802f3b397b879
prakhar <prakharharshit.2003@gmail.com>
2024-08-24 00:32:39 +0530

    changes in port

--- School_Management/src/config/database.js
+++ School_Management/src/config/database.js
@@ -1,14 +1,16 @@
 
-const {Sequelize}=require('sequelize');
-const config = require("../config/config.json")[process.env.NODE_ENV || "development"];
+const { Sequelize } = require("sequelize");
+const config = require("../config/config.json")["development"];
 
 const sequelize = new Sequelize(
-  process.env.DATABASE || config.database,
-  process.env.USERNAME || config.username,
-  process.env.PASSWORD || config.password,
+    process.env.PORT,
+    process.env.DATABASE,
+    process.env.USERNAME,
+    process.env.PASSWORD,
   {
-    host: process.env.HOST || config.host,
-    port: process.env.PORT || config.port || 3306,
-    dialect: process.env.DIALECT || config.dialect,
+    host: config.host,
+    dialect: config.dialect,
   }
 );
+
+module.exports = sequelize;
