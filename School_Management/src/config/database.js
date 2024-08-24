18d6c00754ca4921c1a356836b98df613e096bc5
prakhar <prakharharshit.2003@gmail.com>
2024-08-24 12:02:27 +0530

    some errors resolved

--- School_Management/src/config/database.js
+++ School_Management/src/config/database.js
@@ -1,6 +1,6 @@
 
 const { Sequelize } = require("sequelize");
-const config = require("../config/config.json")["development"];
+const config = require("../config/config.json")[process.env.NODE_ENV];
 
 const sequelize = new Sequelize(
     config.database,    // Database name
